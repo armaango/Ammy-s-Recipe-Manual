ae216ef3cdb0674cbaaaf0ea9dac0b5274f5ef11
armaango <armaan.goyal@gmail.com>
2017-02-03 19:15:10 -0800

    Basic recipe componets done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe';
 
@@ -8,12 +8,17 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  @Output()recipeSelected = new EventEmitter<Recipe>();
   recipe = new Recipe('Dum','Dum','http://www.dummymag.com//media/img/dummy-logo.png');
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(recipe:Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe';
 
 @Component({
   selector: 'arc-recipes',
   templateUrl: './recipes.component.html'
 })
 export class RecipesComponent implements OnInit {
+  selectedRecipe:Recipe; 
 
   constructor() { }
 
